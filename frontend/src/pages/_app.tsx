d831e61696950fdd067a70944f16258d077adcb0
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2025-06-23 22:16:26 -0700

    including header universally

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider>
+    <>
+      <Header />
       <Component {...pageProps} />
-    </ClerkProvider>
+    </>
   );
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,7 @@
-import Header from "../components/Header";
-
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main style={{ padding: 24 }}>
-        <h2>Welcome to the Home Page</h2>
-      </main>
-    </>
+    <main style={{ padding: 24 }}>
+      <h2>Welcome to the Home Page</h2>
+    </main>
   );
 }
