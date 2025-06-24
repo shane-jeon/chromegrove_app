b8bba24c591feaa05fb40815fd67dcc02aea88c9
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2025-06-23 22:16:38 -0700

    removed redundancies

--- frontend/src/pages/sign-in.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: 40 }}>
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-    </div>
-  );
-}
--- frontend/src/pages/sign-up.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function SignUpPage() {
-  return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: 40 }}>
-      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-    </div>
-  );
-}
