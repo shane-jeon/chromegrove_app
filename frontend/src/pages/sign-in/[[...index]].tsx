cd27438f9f8219918d4b0e0020196ff4c3995961
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2025-06-24 21:59:25 -0700

    corrected sign-in user flow so user will NOT be directed to the complete profile page

--- frontend/src/pages/sign-in/[[...index]].tsx
+++ frontend/src/pages/sign-in/[[...index]].tsx
@@ -7,7 +7,7 @@ export default function SignInPage() {
         path="/sign-in"
         routing="path"
         signUpUrl="/sign-up/student"
-        afterSignInUrl="/complete-profile"
+        afterSignInUrl="/dashboard"
       />
     </div>
   );
