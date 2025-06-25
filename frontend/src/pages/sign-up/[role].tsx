73c5649decd7662b11533f21c4209183ec4f83f3
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2025-06-24 18:19:24 -0700

    changed signup page to be dynamic, visiting a sign up based on role on frontend

---
+++ frontend/src/pages/sign-up/[role].tsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { SignUp } from "@clerk/nextjs";
+
+export default function RoleSignupPage() {
+  const router = useRouter();
+  const { role } = router.query;
+
+  useEffect(() => {
+    if (role && typeof role === "string") {
+      localStorage.setItem("signup_role", role);
+    }
+  }, [role]);
+
+  return (
+    <div className="mt-10 flex justify-center">
+      <SignUp path={`/signup/${role}`} routing="path" signInUrl="/sign-in" />
+    </div>
+  );
+}
